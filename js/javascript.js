ba3899126f52b92d556eb99bb478f2b90dcb9c9e
Kelvin Santos <BionicMonkey@users.noreply.github.com>
2021-02-26 18:26:18 -0300

    Página inicial 25%
    Primeira página quase finalizada

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,7 @@ function bannerSwitcher() {
   
   $("nav .controls label").click(function () {
     clearInterval(bannerTimer);
-    bannerTimer = setInterval(bannerSwitcher, 5000);
+    bannerTimer = setInterval(bannerSwitcher, 50000);
   });
   
   ///// Anchor Smooth Scroll /////
@@ -23,4 +23,49 @@ function bannerSwitcher() {
   //     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
   //     return false;
   //   });
+  
+// Calcular el IMC
+// Segons aquesta imatge dir si esta Normal / sobrepes /obesitat
+
+
+function calc() {
+    //pega valores inseridos, e substitui virgula por ponto
+    var peso = document.getElementById("peso").value.replace(/,/g, ".");
+    var altura = document.getElementById("altura").value.replace(/,/g, ".");
+  
+    // valida se os campos não estão vazios
+    if (peso == "" || altura == "") {
+      document.getElementById("message").innerHTML = "preencha os campos";
+      return false;
+    }
+  
+    // calcula IMC
+    var resultado = peso / (altura * altura);
+    var message = "";
+  
+    // verifica situação
+    if (resultado < 17) {
+      message = "Você está muito abaixo do peso";
+    } else if (resultado >= 17 && resultado <= 18.49) {
+      message = "Você está abaixo do peso";
+    } else if (resultado >= 18.5 && resultado <= 24.99) {
+      message = "Seu peso está normal";
+    } else if (resultado >= 25 && resultado <= 29.99) {
+      message = "Você está acima do peso";
+    } else if (resultado >= 30 && resultado <= 34.99) {
+      message = "Você tem obesidade nível I";
+    } else if (resultado >= 35 && resultado <= 39.99) {
+      message = "Você tem Obesidade nível II";
+    } else if (resultado >= 40) {
+      message = "Você tem Obesidade nível III";
+    }
+  
+    // mostra resultado
+    resultado = resultado.toFixed(2).replace(/\./g, ",");
+    document.getElementById("message2").innerHTML =
+    "Veja seu reaultado clicando em '2 passo 👇'";
+
+    document.getElementById("message").innerHTML =
+      "Seu IMC é : " + resultado + "<br>" + message;
+  }
   
